ea40303e1108072b0cf4cc718fd15ccb8508c404
Marjorie Simon <marjoriesimon13@gmail.com>
2021-02-18 16:07:49 +0100

    create cocktail

---
+++ app/controllers/cocktails_controller.rb
@@ -0,0 +1,15 @@
+class CocktailsController < ApplicationController
+  def index
+    @cocktails = Cocktail.all
+  end
+  def show
+    @cocktails = Cocktail.find(params[:id])
+  end
+  def new
+    @cocktail = Cocktail.new
+  end
+  def create
+    @cocktail = Cocktail.new(params[:cocktail])
+    @cocktail.save
+  end
+end
