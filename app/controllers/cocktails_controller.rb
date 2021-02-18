f76d65cc5f4dc374b50d5e26d2a7a80c2e77dfd2
Marjorie Simon <marjoriesimon13@gmail.com>
2021-02-18 16:52:32 +0100

    a bit of formatting

--- app/controllers/cocktails_controller.rb
+++ app/controllers/cocktails_controller.rb
@@ -9,7 +9,15 @@ class CocktailsController < ApplicationController
     @cocktail = Cocktail.new
   end
   def create
-    @cocktail = Cocktail.new(params[:cocktail])
+    @cocktail = Cocktail.new(cocktail_params)
     @cocktail.save
+
+    redirect_to cocktail_path(@cocktail)
+  end
+
+  private
+
+  def cocktail_params
+    params.require(:cocktail).permit(:name)
   end
 end
