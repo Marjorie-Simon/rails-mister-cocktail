6058e1f4f51bb505a293b97235e52b34d0f299f4
Marjorie Simon <marjoriesimon13@gmail.com>
2021-02-18 12:59:02 +0100

    odels validations and associations

--- app/models/ingredient.rb
+++ app/models/ingredient.rb
@@ -1,2 +1,7 @@
 class Ingredient < ApplicationRecord
+  #has_many :doses, dependent: :destroy
+  has_many :cocktails, through: :doses
+  has_many :doses
+  validates :name, uniqueness: true
+  validates :name, length: { minimum: 1 }
 end
